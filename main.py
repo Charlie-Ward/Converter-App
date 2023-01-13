5257e351469e4d57018493fa6f332b0b2423f1df
Charlie WARD <59178341+Charlie-Ward@users.noreply.github.com>
2023-01-13 13:52:17 +0000

    changed code

--- main.py
+++ main.py
@@ -1,36 +1,56 @@
 import PySimpleGUI as sg
 
+OptionValues = ['WEIGHT' ,'kg to pounds','pounds to kg' ,'DISTANCE' , 'km to mile','mile to km' ,"TIME" ,'min to sec' , "sec to min", 'TEMPERATURE', '°C to °F', '°F to °C']
+
+NotAllowedOptions = ["SELECT UNITS", "WEIGHT", "DISTANCE", "TIME", "TEMPERATURE"]
+
 layout = [
-    [sg.Text('Converter')],
-    [sg.Input(key = 'NumberInput'), sg.Spin(['km to mile', 'kg to pounds', 'sec to min'], key = 'SelectorInput'),
-     sg.Button('Convert', key = 'Button1')],
-    [sg.Text('Converted Product will appear hear', key = 'OutputText')]
+	[sg.Text("Converting App")],
+	[sg.Input(key = "numberInput"), sg.OptionMenu(values = OptionValues, key = "selectionInput", default_value = "SELECT UNITS"), sg.Button('Convert', key = 'Button1')],
+	[sg.Text('Converted Product will appear hear', key = 'OutputText')]
 ]
 
-window = sg.Window('Converter', layout)
+window = sg.Window("Converter", layout)
 
 while True:
-    event, values = window.read()
-
-    if event == sg.WIN_CLOSED:
-        break
-
-    if event == 'Button1':
-        input_value = values['NumberInput']
-        convert_type = values['SelectorInput']
-        if input_value.isnumeric():
-            if convert_type == 'km to mile':
-                output = round(float(input_value) * 0.6214, 2)
-                output_string = f'{input_value} km are {output} miles.'
-            elif convert_type == 'kg to pounds':
-                output = round(float(input_value) * 2.205, 2)
-                output_string = f'{input_value} kg are {output} pounds.'
-            elif convert_type == 'sec to min':
-                output_mins = round(float(input_value) // 60, 1)
-                output_secs = round(float(input_value) % 60, 1)
-                output_string = f'{input_value} secs are {output_mins} mins and {output_secs} secs.'
-            window['OutputText'].update(output_string)
-        else:
-            window['OutputText'].update('Please enter a number')
-
-window.close()
+	event, values = window.read()
+	
+	if event == sg.WIN_CLOSED:
+		break
+	if event == "Button1":
+		inputValue = values['numberInput']
+		inputType = values['selectionInput']
+		for i in range(len(NotAllowedOptions)):
+			if inputType == NotAllowedOptions[i]:
+				window['OutputText'].update("Please select a correct unit type")
+				break
+		if inputValue == "":
+			window['OutputText'].update('Please enter a number')
+		else:
+			if inputType == 'kg to pounds':
+				Converted = round(float(inputValue) * 2.205, 1)
+				ConvertText = f'{inputValue}kg = {Converted} pound'
+				window['OutputText'].update(ConvertText)
+			elif inputType == 'pounds to kg':
+				Converted = round(float(inputValue)/ 2.205, 1)
+				ConvertText = f'{inputValue} pound = {Converted}kg'
+				window['OutputText'].update(ConvertText)
+			elif inputType == "km to mile":
+				Converted = round(float(inputValue)/ 1.609, 1)
+				ConvertText = f'{inputValue}km = {Converted} mile'
+				window['OutputText'].update(ConvertText)
+			elif inputType == "mile to km":
+				Converted = round(float(inputValue)* 1.609, 1)
+				ConvertText = f'{inputValue} mile = {Converted}km'
+				window['OutputText'].update(ConvertText)
+			elif inputType == "min to sec":
+				Converted = round(float(inputValue) * 60, 1)
+				ConvertedText = f'{inputValue} minutes = {Converted} seconds'
+				window['OutputText'].update(ConvertedText)
+			elif inputType == "sec to min":
+				ConvertedMins = round(float(inputValue) // 60, 1)
+				ConvertedSecs = round(float(inputValue) % 60, 1)
+				Converted = f'{inputValue} seconds = {ConvertedMins} minutes and {ConvertedSecs} seconds'
+				window['OutputText'].update(Converted)
+			elif inputType == "°C to °F":
+				Converted = round(float(inputValue)) /* need to finish */
